163777985a499903feb12671c17ccf2dd0825a99
Sam Marinovich <s.marinovich@student.unsw.edu.au>
2024-10-29 20:42:30 +1100

    script setup

---
+++ s/script/cookies.js
@@ -0,0 +1,25 @@
+// Cookie functions taken from https://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue) {
+    const d = new Date();
+    d.setTime(d.getTime() + (90 * 24 * 60 * 60 * 1000)); // Files will expire in 1 month
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+export { setCookie, getCookie };
